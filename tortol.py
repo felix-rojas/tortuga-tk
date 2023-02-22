614f2cb9a2fe2f24a4d5ae8d71fcca7a958a7c2d
felix-rojas <a01201946@tec.mx>
2023-02-22 16:04:34 -0600

    Window with text input, doesnt read input

--- tortol.py
+++ tortol.py
@@ -1,17 +1,26 @@
 from tkinter import *
 
+# window instance
 top = Tk()
 
-
+# drawing canvas
 C = Canvas(top, bg = "white", height = 500, width = 500)
-coord = 10, 50, 240, 210
-arc = C.create_arc(coord, start = 0, extent = 150, fill = "red")
-line = C.create_line(10,10,200,200,fill = 'white')
 C.pack()
 
+# label for input
 L1 = Label(top, text = "Comando")
 L1.pack(side = LEFT)
-E1 = Entry(top, bd = 5)
+
+# text
+userInputs = Text(top)
+
+# input field
+sendButton = Button(top, text="Get input")
+sendButton.pack(side = RIGHT)
+
+E1 = Entry(top, bd = 5, textvariable = userInputs)
 E1.pack(side = RIGHT)
 
+
+# window loop
 top.mainloop()
